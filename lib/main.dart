c06c3c5e358a88e24fbbdf7807d427dfe9d8759b
Sriram Kiran Senthilkumar <sriramkiran.senthilkumar@syncfusion.com>
2020-04-28 21:22:57 +0530

    committed

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_core/core.dart';
+
+void main() {
+  // Register your license here
+  SyncfusionLicense.registerLicense(null);
+  return runApp(DarkThemeChart());
+  }
+
+class DarkThemeChart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chart in Dark Theme',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,   
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Chart in Dark Theme'),
+        ),
+        body: SfCartesianChart(
+            primaryXAxis: CategoryAxis(),
+            primaryYAxis: NumericAxis(
+              minimum: 3,
+              maximum: 12,
+              interval: 2
+            ),
+            // Chart title
+            title: ChartTitle(text: 'Analysis of Share Market'),
+            // Enable legend
+            legend: Legend(isVisible: true),
+            // Enable tooltip
+            tooltipBehavior: TooltipBehavior(enable: true),
+            series: <LineSeries<SalesData, String>>[
+              LineSeries<SalesData, String>(
+                  dataSource: <SalesData>[
+                    SalesData('Jan', 6.6),
+                    SalesData('Feb', 7.3),
+                    SalesData('Mar', 8.7),
+                    SalesData('Apr', 3.4),
+                    SalesData('May', 9.8)
+                  ],
+                  xValueMapper: (SalesData sales, _) => sales.year,
+                  yValueMapper: (SalesData sales, _) => sales.sales,
+                  // Enable data label
+                  dataLabelSettings: DataLabelSettings(isVisible: true))
+            ]));
+  }
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
+}
