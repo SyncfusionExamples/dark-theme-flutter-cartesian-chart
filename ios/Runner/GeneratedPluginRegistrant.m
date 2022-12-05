d344c6c2bc08dc0af13390e65d08e3c04493b44b
Devi Aruna <deviaruna.murugan@syncfusion.com>
2022-12-05 17:30:34 +0530

    FLUT-7109-[bug][flutter]: Done sample canges

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -2,13 +2,13 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #import "GeneratedPluginRegistrant.h"
-#import <url_launcher/UrlLauncherPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
 }
 
 @end
